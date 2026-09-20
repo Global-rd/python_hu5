0cbbc87a0e8a9915e52e2cf033505660f151456f
jootamas <info@jootamas.eu>
2026-09-20 11:50:13 +0200

    homework-1

---
+++ homeworks/jootamas/hw_01_introduction/main.py
@@ -0,0 +1,21 @@
+"""
+This is my first Python program
+"""
+
+# string
+print( "Hello World!" )
+
+# int
+print( 2026 )
+
+# float
+print( 3.14 )
+
+# bool true
+print( True )
+
+# bool false
+print( False )
+
+# none
+print( None )
