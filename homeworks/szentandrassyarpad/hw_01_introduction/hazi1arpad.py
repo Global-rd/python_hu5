058191fea16726ded2cf78549fa22d78bc5863b5
aszentan <arpad.szentandrassy@gmail.com>
2026-09-20 22:06:57 +0200

    Szent-Andrássy Árpád: első házi kész

---
+++ homeworks/szentandrassyarpad/hw_01_introduction/hazi1arpad.py
@@ -0,0 +1,10 @@
+"""
+This is my first 
+Python program
+"""
+
+print(1)                #Integer
+print(2.222)            #Float
+print("Szép napot!")    #String
+print(True)             #Boolean
+print(None)             #None
