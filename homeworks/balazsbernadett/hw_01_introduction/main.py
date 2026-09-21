99ac640a0e19159ae4b8109dd8dfd7f7a4e50abf
Betti333 <balazsbetti333@gmail.com>
2026-09-21 20:58:56 +0200

    homework 01 done (primitive data types)

---
+++ homeworks/balazsbernadett/hw_01_introduction/main.py
@@ -0,0 +1,9 @@
+"""This is my first Python program"""
+
+print("Első házim") #string
+print("333")        #string
+print(333)          #integer
+print(3.14)         #float
+print(True)         #boolean
+print(False)        #boolean
+print(None)         #NoneType
