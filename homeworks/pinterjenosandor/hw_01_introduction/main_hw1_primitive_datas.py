49f6bf342645f8dd41478c2eb4ef61edf6e16e28
Pintér Jenő <pinter.jeno@bumjin.hu>
2026-09-21 09:10:24 +0200

    Update main_hw1_primitive_datas.py
    Javítva

--- homeworks/pinterjenosandor/hw_01_introduction/main_hw1_primitive_datas.py
+++ homeworks/pinterjenosandor/hw_01_introduction/main_hw1_primitive_datas.py
@@ -4,18 +4,18 @@ line by line
 with comments,examples and it wait a little between the data types. i use for wait: import and time
 '''
 import time
-print ("Stirng type, name string") #szöveg, vagy karakterlánc
-print ("string example: Hello! Egyszerű szöveg vagyok, készített Pintér Jenő")
+print("String type, name string") #szöveg, vagy karakterlánc
+print("string example: Hello! Egyszerű szöveg vagyok, készített Pintér Jenő")
 time.sleep(1)
-print ("Integer type, name integer or int") #egész számok
-print ("integer example: 5")
+print("Integer type, name integer or int") #egész számok
+print(5)
 time.sleep(1)
-print ("Float type, name Float") # lebegőpontos számok, melyek már tartalmaznak tizedesjegyeke
-print ("float example: 3,14")
+print("Float type, name Float") # lebegőpontos számok, melyek már tartalmaznak tizedesjegyeke
+print(3,14)
 time.sleep(1)
-print ("bolean type, name bolean") #logikai adattípus, fontos a nagy betű, két kimenete van a:
-print("True or False")
+print("bolean type, name bolean") #logikai adattípus, fontos a nagy betű, két kimenete van a:
+print(True)
 time.sleep(1)
 print("None type, name None") #ezt használjuk pl. hibakeresésre, hamis értéket ad vissza
-print("False")
+print(False)
 time.sleep(1)
