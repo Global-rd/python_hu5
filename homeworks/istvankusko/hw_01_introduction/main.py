65a2357a2803f242863971f6f8d39226c112dfc0
kuskoi <kuskoi@protonmail.com>
2026-09-22 22:00:42 +0200

    Create main.py

---
+++ homeworks/istvankusko/hw_01_introduction/main.py
@@ -0,0 +1,29 @@
+""" This is Istvan's first Python program.
+This is a good training session, and I hope I can learn the basics. """
+
+import time
+
+print("Starting...")
+time.sleep(2)
+
+print("\n")
+egesz=42
+print("egesz szam:")
+print(egesz)    #egesz szam
+#print(type(egesz))
+print("------------\n")
+lebeg=3.14  
+print("lebegopontos szam:")
+print(lebeg)    # lebegopontos
+print("------------\n")
+print("String:")
+szoveg="This is first string" 
+print(szoveg)   # string
+print("------------\n")
+print("bool:")
+elso_var = 5
+masodik_var = 10
+print(elso_var > masodik_var) # bool - False
+print(elso_var < masodik_var) # bool - True
+print("\n")
+print("Bye Bye :-) \n")
