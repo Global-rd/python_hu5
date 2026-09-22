68aae2bbfa5238e7442e86ad1fcb300bad03db8c
Gabrielbks07 <332452360+Gabrielbks07@users.noreply.github.com>
2026-09-22 13:29:25 +0100

    Add homework 1 introduction

---
+++ homeworks/bakacsygabor/hw-01/homework.py
@@ -0,0 +1,9 @@
+'''
+This is my first Python homework
+Author: Gabor Bakacsi
+'''
+print("Hello World!")   # this is a string that will be printed to the console
+print(2026)            # this is an integer that will be printed
+print(5.6)             # this is a float that will be printed
+print(True)            # this is a boolean that will be printed
+print(None)            # this is a NoneType that will be printed
