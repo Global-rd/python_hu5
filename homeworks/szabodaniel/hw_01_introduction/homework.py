29e35636bbe2d712c64cd5ef64eb0179291ed6d3
prodotDANI <szabodaniel.net@gmail.com>
2026-09-23 22:49:46 +0200

    ora2hw_primitiv_adattipus
    összes primitiv adattíus.

---
+++ homeworks/szabodaniel/hw_01_introduction/homework.py
@@ -0,0 +1,29 @@
+"""
+This is 
+my first 
+python program
+"""
+
+'''
+This is 
+my first 
+python program
+'''
+
+#STRING - KARAKTERLÁNC
+print("apple")
+print("1 egy")
+
+#INTEGER - EGÉSZ SZÁMOK
+print(1)
+print(314)
+
+#FLOAT - LEBEGŐPONTOS SZÁM
+print(3.14)
+
+#BOOLEAN - LOGIKAI ÉRTÉKEK
+print(True)
+print(False)
+
+#NONE TYPE - INFORMÁCIÓ HIÁNYA
+print(None)
