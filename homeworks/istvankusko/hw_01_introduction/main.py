fd77d51de902bcd7108418982bc055d6e3b53954
kuskoi <kuskoi@protonmail.com>
2026-09-23 21:40:47 +0200

    Update main.py

--- homeworks/istvankusko/hw_01_introduction/main.py
+++ homeworks/istvankusko/hw_01_introduction/main.py
@@ -1,29 +1,28 @@
 """ This is Istvan's first Python program.
 This is a good training session, and I hope I can learn the basics. """
 
-import time
+import time   #load time module
 
 print("Starting...")
-time.sleep(2)
+time.sleep(2)  #program is waiting 2 sec
 
 print("\n")
-egesz=42
-print("egesz szam:")
-print(egesz)    #egesz szam
-#print(type(egesz))
+integer_variable=42
+print("This is an interger:")
+print(integer_variable)    #Integer
 print("------------\n")
-lebeg=3.14  
-print("lebegopontos szam:")
-print(lebeg)    # lebegopontos
+floating_variable=3.14  
+print("This is floating number:")
+print(floating_variable)    #floating 
 print("------------\n")
 print("String:")
-szoveg="This is first string" 
-print(szoveg)   # string
+string_variable="This is first string" 
+print(string_variable)   #string
 print("------------\n")
 print("bool:")
-elso_var = 5
-masodik_var = 10
-print(elso_var > masodik_var) # bool - False
-print(elso_var < masodik_var) # bool - True
+logical_first_variable = 5
+logical_second_variable = 10
+print(logical_first_variable > logical_second_variable) # bool - False
+print(logical_first_variable < logical_second_variable) # bool - True
 print("\n")
 print("Bye Bye :-) \n")
