6a5f19798b24d2ec789cb4eb73bfbfcd2863a3f1
jipacs-del <jipacs@gmail.com>
2026-09-24 19:17:55 +0200

    Elkészült az 1. házi feladat

---
+++ homeworks/ipacsjanos/hw_01_introduction/homework.py
@@ -0,0 +1,15 @@
+"""
+This is my first Python program
+"""
+
+# Egész szám (Integer)
+print(42)  # int
+
+# Tizedes tört / Lebegőpontos szám (Float)
+print(3.14)  # float
+
+# Szöveg / Karakterlánc (String)
+print("Szia, Python!")  # str
+
+# Logikai érték (Boolean)
+print(True)  # bool
